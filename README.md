b617b595f42e59072ff806790758537e6ec8acf7
JonKerridge <jonkerridge@btinternet.com>
2018-04-18 08:56:37 +0100

    initial commit from local folder IJLibrary

---
+++ README.md
@@ -0,0 +1,20 @@
+
+
+The groovyJCSP library provides a set of Groovy helper classes that make the use of the JCSP library much easier by reducing the amount of code that has to be written to build parallel systems.<p>
+
+
+The library uses the @CompileStatic Groovy annotation to improve performance of the library.
+
+These classes are used extensively in the books<p>
+"Using Concurrency and Parallelism Effectively parts i & ii"
+by Jon Kerridge published by Bookboon, free of charge.<p>
+
+http://bookboon.com/en/using-concurrency-and-parallelism-effectively-i-ebook<p>
+http://bookboon.com/en/using-concurrency-and-parallelism-effectively-ii-ebook<p>
+
+
+
+It is published using the Apache 2.0 Licence.
+
+
+Contact J.Kerridge@napier.ac.uk
---
+++ src/groovyJCSP/util/TestUtilities.groovy
@@ -0,0 +1,56 @@
+    //////////////////////////////////////////////////////////////////////
+    //                                                                  //
+    //  groovyJCSP (groovy support for "CSP for Java") Libraries        //
+    // Copyright 2005-2017 Jon Kerridge                                 //
+    //                                                                  //
+    // Licensed under the Apache License, Version 2.0 (the "License");  //
+    // you may not use this file except in compliance with the License. //
+    // You may obtain a copy of the License at                          //
+    //                                                                  //
+    //      http://www.apache.org/licenses/LICENSE-2.0                  //
+    //                                                                  //
+    // Unless required by applicable law or agreed to in writing,       //
+    // software distributed under the License is distributed on         //
+    // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,  //
+    // either express or implied. See the License for the specific      //
+    // language governing permissions and limitations under the License.//
+    //                                                                  //
+    //  Author contact: J.Kerridge@napier.ac.uk                         //
+    //                                                                  //
+    //////////////////////////////////////////////////////////////////////
+
+package groovyJCSP.util
+
+
+abstract class TestUtilities {
+
+  static boolean  listContains (list1, list2) {
+
+    if (list1.size != list2.size) {
+      return false
+    }
+    else {
+      list1.sort()
+      list2.sort()
+      return (list1 == list2)
+    }
+  } // end listContains
+
+  static boolean list1GEList2 (list1, list2) {
+
+    if (list1.size != list2.size) {
+      return false
+    }
+    else {
+      for (i in 0 ..< list1.size ) {
+        if (list1[(int)i] < list2[(int)i]) {
+          return false
+        }
+      }
+      return true
+    }
+
+  }
+
+
+}
